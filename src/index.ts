44bcadbdc3b83c01cfe982e3bc255ca5da61151e
Alexander Tananica <alexander.tananica@collow.nl>
2024-07-15 16:32:38 +0200

    added createSwatches to exports

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export { default as ColorPicker } from "./components/ColorPicker";
 export type { SwatchValue, Mode, PaletteConfig } from "./types";
 
 export { createRandomPalette } from "./lib/createRandomPalette";
+export { createSwatches } from "./lib/createSwatches";
 
 export { DEFAULT_STOP, MODES, DEFAULT_PALETTE_CONFIG } from "./lib/constants";
